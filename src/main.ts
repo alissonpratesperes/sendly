ed96c98d00408a89d9a1c81d161c65a3584ad8f7
Alisson Prates Peres <a.pratesperes@gmail.com>
2026-08-18 21:26:27 -0300

    Added SwaggerDocs for API endpoints documentation since start of the project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDocumentation = new DocumentBuilder()
+    .setTitle('Sendly')
+    .setDescription('API Endpoints Documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerDocumentation);
+
+  SwaggerModule.setup('swagger', app, swaggerDocument, {
+    swaggerOptions: {
+      tagSorter: 'alpha',
+      operationSorter: 'alpha',
+    },
+  });
 
   await app.listen(process.env.PORT ?? 3333);
 }
